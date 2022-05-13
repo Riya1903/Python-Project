2d1ace2722d4b656742fad0469ba42dd097d1e3a
Riya Pawar <100956741+pawarriya@users.noreply.github.com>
2022-05-13 11:24:28 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,15 @@
+print("this script is intended to swap the values of two data variable using a third temporary variable")
+x=input("enter the first no.\integer:")
+y=input("enter the second no.")
+print(" values before swapping")
+print("x="+x)
+print("y="+y)
+t=x
+x=y
+y=t
+print(" values after swapping")
+print("x="+str(x))
+print("y="+str(y))
+
+
+
