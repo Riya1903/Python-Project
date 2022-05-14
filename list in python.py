bc312201e7b80c384de34a228049655f74d84a6a
Riya Pawar <100956741+pawarriya@users.noreply.github.com>
2022-05-14 18:46:14 +0530

    Add files via upload

---
+++ list in python.py
@@ -0,0 +1,13 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+#length of the list
+print(len(thislist))
+#type checking
+print(type(thislist))
+#access the list
+list=["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(list[2:5])
+print(list[:5])
+print(list[5:])
+print(list[-4:-1])
+
