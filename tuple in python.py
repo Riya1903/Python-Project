f4844957ebaff672c27e6edaba0dee41b4ae364a
Riya Pawar <100956741+pawarriya@users.noreply.github.com>
2022-05-14 19:30:30 +0530

    Add files via upload

---
+++ tuple in python.py
@@ -0,0 +1,12 @@
+thistouple=("apple", "banana", "cherry")
+print(thistouple)
+#length of the list
+print(len(thistouple))
+#type checking
+print(type(thistouple))
+#access the list
+touple=("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(touple[2:5])
+print(touple[:5])
+print(touple[5:])
+print(touple[-4:-1])
---
+++ update tuple.py
@@ -0,0 +1,12 @@
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+#join tuple
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
