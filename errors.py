b3f58571a22751dfab248cb6a60877caaa9f1022
Riya Pawar <100956741+pawarriya@users.noreply.github.com>
2022-05-14 11:15:30 +0530

    Add files via upload

---
+++ errors.py
@@ -0,0 +1,85 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("hello world")
+hello world
+Print("hello world")
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    Print("hello world")
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+print()
+
+print()
+
+print("riya pawar")
+riya pawar
+print("riya pawar\n m.tech(IT)\n contact no.")
+riya pawar
+ m.tech(IT)
+ contact no.
+print("riya pawar)
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print('hello world')
+      
+hello world
+print(riya pawar")
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print(riya pawar)
+      
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print(riya)
+      
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    print(riya)
+NameError: name 'riya' is not defined
+print("is")
+      
+is
+print(is)
+      
+SyntaxError: invalid syntax
+a=10
+      
+b=20
+      
+c=a+b
+      
+print("c")
+      
+c
+print(c)
+      
+30
+riya50
+      
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    riya50
+NameError: name 'riya50' is not defined
+riya=50
+      
+riya
+      
+50
+23riya=50
+      
+SyntaxError: invalid decimal literal
+23_riya=50
+      
+SyntaxError: invalid decimal literal
+_riya=50
+      
+var_a=a
+      
+print(var_a)
+      
+10
+var_a=z
+      
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    var_a=z
+NameError: name 'z' is not defined
